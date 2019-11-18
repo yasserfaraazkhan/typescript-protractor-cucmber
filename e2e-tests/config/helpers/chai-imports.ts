9c8e57f32fd879f1f988579fd606b7ad9500e429
Lavu_India_QA1 <lavu_india_qa1@ET-LV-CAB-0026.local>
2019-11-19 03:16:12 +0530

    cleared package-lock

--- e2e-tests/config/helpers/chai-imports.ts
+++
@@ -1,6 +0,0 @@
-import * as chaiExpect from 'chai';
-import * as cap from 'chai-as-promised';
-
-chaiExpect.use(cap);
-
-export const expect = chaiExpect.expect;
--- e2e-tests/config/helpers/cucumber.config.ts
+++
@@ -1,5 +0,0 @@
-import { defineSupportCode } from 'cucumber';
-
-defineSupportCode(({setDefaultTimeout}) => {
-    setDefaultTimeout(10000);
-});
